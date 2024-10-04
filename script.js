0c75cd6e0c55c3113f3cb2091d90bbae401a77a7
soliloquyOS <marceline.mail2@gmail.com>
2024-10-04 14:04:27 -0600

    mobile improvements & bug fixes

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
   const menuToggle = document.getElementById('menu-toggle');
-  const sidebar = document.getElementById('sidebar');
+  const mobileNav = document.getElementById('mobile-nav');
 
   menuToggle.addEventListener('click', function () {
-    sidebar.classList.toggle('active');
+    mobileNav.style.display =
+      mobileNav.style.display === 'block' ? 'none' : 'block';
   });
 
   document.addEventListener('click', function (event) {
     const isClickInside =
-      sidebar.contains(event.target) || menuToggle.contains(event.target);
-    if (!isClickInside && sidebar.classList.contains('active')) {
-      sidebar.classList.remove('active');
+      mobileNav.contains(event.target) || menuToggle.contains(event.target);
+    if (!isClickInside && mobileNav.style.display === 'block') {
+      mobileNav.style.display = 'none';
     }
   });
 });
